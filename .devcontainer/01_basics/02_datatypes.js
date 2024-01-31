0cb75fcb6fec2d25e0a695e981b2798e87989d78
Zeeshan626626 <133057951+Zeeshan626626@users.noreply.github.com>
2024-01-31 08:13:22 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,10 @@
+let name="shan"         //that is string data types
+let age=19               //that is int data types
+let pakstje= false  // that is bolean data types
+let zeeshan;            //that is undefined data types
+
+// null is a standalone value and object data types 
+// symbol is a unique 
+
+console.log(typeof "shan");  // use for cheking datatypes
+
