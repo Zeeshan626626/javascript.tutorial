63e46666a86a180a09df15c459b676d0e694f313
Zeeshan626626 <133057951+Zeeshan626626@users.noreply.github.com>
2024-01-31 11:54:06 +0000

    dataTypes-summry.js

---
+++ .devcontainer/01_basics/datatypes-summry.js
@@ -0,0 +1,60 @@
+
+
+//*********************************data types Summry*************************/
+
+
+// how to store data in memory and how to access it.
+// according to this we divided dataTypes into its two types 
+
+// permitive and non-permitive(Refrence)
+
+//****************permitive*********************//
+
+// it has 7 types 
+
+//1- string
+ let name ="shan"
+
+//2- number
+ let age =19
+
+ //3- boolean
+ let shan= true or false
+
+ //4- null
+ let score=null    // it is not empty
+
+ //5- undefindes
+ let abc;
+
+ //6- symbol 
+ const id=Symbol('123')   // it is used to make anything 'unique'
+
+ //7- bigNumber
+ let bigNumber= 123424234234n   // it is use for big number  
+
+
+ //***********************non-permitive or Refrence *********************//
+
+ //it has three types that are 
+
+ //1   array
+ //we write the array structure in this formate that are write brlow
+   const shan1=["kia","hal","hain","jee"];
+
+
+ //2   objects
+  // we write the object in between the {} that brekuts
+  let myobj={
+    name:"ali",
+    age:12,
+  }
+
+
+ //3   functions
+   let myfunction = function(){
+    console.log("hellow word");
+   }
+
+   //  console.log(typeOf myobj);    used to chek the datatypes                 
+ 
