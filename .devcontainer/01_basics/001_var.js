898c8acdc2504cdbde0624b4220bc62509f462e4
Zeeshan626626 <133057951+Zeeshan626626@users.noreply.github.com>
2024-01-31 07:42:57 +0000

    var and constant

---
+++ .devcontainer/01_basics/001_var.js
@@ -0,0 +1,13 @@
+const accountId=2323    
+/*
+constant cannat be changed and we prefer to use the let insted of var becuase of black 
+scope and functional scope and let and var values changed 
+*/
+var accountName="shan"
+let accountCity="multan"
+accountKhan="zeeshan"
+ accountName=""
+//accountId=239993
+let khan;
+
+console.table([accountId,accountName,accountKhan,accountCity]);
