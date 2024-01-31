26abdae1fc28b720d8a519c1bcd4a143497699a6
Zeeshan626626 <133057951+Zeeshan626626@users.noreply.github.com>
2024-01-31 10:32:07 +0000

    converstion of data types

---
+++ .devcontainer/01_basics/03_convrstionoperation.js
@@ -0,0 +1,20 @@
+let isLoggin=0
+let boleanloggedin=Boolean(isLoggin)
+console.log(boleanloggedin);
+// when we convert number in to boolean
+//1=> true 
+//0=>false
+//""=> false
+//"shan"=>true
+
+
+let score = "false"
+let convertnumberis=Number(score)
+console.log(convertnumberis);
+
+// when we convert above above
+// "shan"=> NaN
+// "33"=>33
+//"33abc"=>NaN
+
+// and also same we convert number into string and string into number 
