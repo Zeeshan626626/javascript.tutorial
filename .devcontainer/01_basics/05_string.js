ef687493afe2090051c0cc6a4bc411f586e044cf
Zeeshan626626 <133057951+Zeeshan626626@users.noreply.github.com>
2024-02-01 07:06:11 +0000

    strings

---
+++ .devcontainer/01_basics/05_string.js
@@ -0,0 +1,18 @@
+const name="shan"
+const age = 20
+console.log(`my name is ${name} and also my age is ${age}`);
+
+const newGame= new String('pubg')
+console.log(newGame);
+console.log(newGame[0]);  // in '0' position is 'p'
+console.log(newGame.length); // to check the leanth of the string
+console.log(newGame.toUpperCase()); // use to make all words capital
+console.log(newGame.charAt(3));  // chek the position of the words 
+
+const newString="  shan  "
+console.log(newString)
+console.log(newString.trim())  // trim is use to remove the spaces
+
+const url="https://shan.com/shan24%sanga"
+console.log(url.replace('%20','-'))  // use to replace the words to anything
+console.log(url.includes('shan')) //chek that words avilable or not 
